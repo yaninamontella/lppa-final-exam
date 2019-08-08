c4d22a7d8001a512fe9fcf20d03986159a03dbfb
yaninamontella <yani_montella@hotmail.com>
2019-08-07 21:41:21 -0300

    delete blanks before the equal sign

--- js/world.js
+++ js/world.js
@@ -9,14 +9,14 @@ var board=[
 [, ,{value:1},{value:1}, {value:1}, , ,]
 ]
 
-var selectedPeg={x:undefined, y:undefined}
+var selectedPeg= {x:undefined, y:undefined}
 
-var suggestions=[]
+var suggestions= []
 
-var score=0
+var score= 0
 
-var suggestions=[]
+var suggestions= []
 
-var posibilities=1
+var posibilities= 1
 
-var scoreTable=[]
+var scoreTable= []
